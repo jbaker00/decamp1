67b52ee5c71cc16c95e731d2cea8d441d00002b6
Baker, James <jambak@freeip.amazon.com>
2018-02-01 13:18:52 -0800

    added the output table and view controller

---
+++ DeCampV3/TableViewControllerBusList.h
@@ -0,0 +1,16 @@
+//
+//  TableViewControllerBusList.h
+//  DeCampV3
+//
+//  Created by Baker, James on 2/1/18.
+//  Copyright © 2018 Baker, James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewControllerBusList : UITableViewController
+{
+    @public NSArray *tblData;
+    @public NSString *tblSectionName;
+}
+@end
