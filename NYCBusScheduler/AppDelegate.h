0d71aaeada13bf3ae6c840616328c0c5b4d26610
James Baker <jbaker00@gmail.com>
2018-02-28 23:06:39 -0800

    added word wrap

---
+++ NYCBusScheduler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeCampV3
+//
+//  Created by Baker, James on 10/23/17.
+//  Copyright © 2017 Baker, James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NYCBusScheduler/StopsTableVC.h
@@ -0,0 +1,43 @@
+//
+//  StopsTableVC.h
+//  DeCampV3
+//
+//  Created by James Baker on 2/10/18.
+//  Copyright © 2018 Baker, James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+
+@interface StopsTableVC : UITableViewController
+{
+    
+    //Array of bus stops
+    @public NSArray *tblStopData;
+    //Name of the bus selected
+    @public  NSString *tblFromSectionName;
+    
+    //public version of the bus numbers
+    @public NSMutableArray *tblBusList;
+    
+    //NSMutableDictionary *NYCPABus;
+    //NSMutableDictionary *ThirtyTwoBus;
+    //NSMutableDictionary *ThirtyThreeBus;
+    //NSMutableDictionary *FortyForyBus;
+    //NSMutableDictionary *SixtySixBus;
+    //NSMutableDictionary *EightyEighBus;
+    //NSMutableDictionary *NintyNineBus;
+    NSMutableDictionary *BusDict;
+    
+    NSArray *busSectionTitles;
+    
+    NSArray *stringArrayBusStop;
+    
+    
+}
+
+//Name of bus selected in property
+//@property (strong, nonatomic) NSString *selectedBus;
+@property (nonatomic) bool from;
+@end
