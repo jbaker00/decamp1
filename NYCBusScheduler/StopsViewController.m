d8a7e3b45261ddc61fbfbd5b5e7dfbf2bc10f7fa
James Baker <jbaker00@gmail.com>
2018-04-24 19:58:42 -0700

    Google ads only and going with larger ad for google

---
+++ NYCBusScheduler/StopsViewController.m
@@ -0,0 +1,37 @@
+//
+//  StopsViewController.m
+//  NYCBusScheduler
+//
+//  Created by James Baker on 4/24/18.
+//  Copyright © 2018 Baker, James. All rights reserved.
+//
+
+#import "StopsViewController.h"
+
+@interface StopsViewController ()
+
+@end
+
+@implementation StopsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
